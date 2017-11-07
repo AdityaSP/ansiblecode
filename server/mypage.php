6632903c433c602421d39945063654d99eadf192
Aditya <sp.aditya@gmail.com>
2017-11-07 09:12:26 +0530

    code for ansible demos

---
+++ server/mypage.php
@@ -0,0 +1,13 @@
+<?php
+ $conn = mysqli_connect("db","demo","demo","demo");
+ if (!$conn) {
+	die("Connection failed");
+ }
+ echo "Connected Successfully";
+ $res = mysqli_query($conn, "select * from t1");
+ while ($row = mysqli_fetch_assoc($res)){
+	echo "<h1> " . $row["name"] . "</h1>";
+ }
+ echo "Pitching in from " . gethostname();
+
+?>
